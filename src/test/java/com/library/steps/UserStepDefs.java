294005cfa06a67018f654c92c03e1cab706f10bc
Senai <simoseni@gmail.com>
2023-08-09 09:37:31 -0700

    StepDef2 Done

--- src/test/java/com/library/steps/UserStepDefs.java
+++ src/test/java/com/library/steps/UserStepDefs.java
@@ -6,6 +6,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
+import java.util.List;
+
 public class UserStepDefs {
     String actualUserCount;
     @Given("Establish the database connection")
@@ -36,4 +38,22 @@ public class UserStepDefs {
         System.out.println("----CONNECTION WILL BE DONE WITH AFTER HOOK");
     }
 
+    List<String> actualColumnList;
+    @When("Execute query to get all columns")
+    public void execute_query_to_get_all_columns() {
+        String query="select * from users";
+
+        DB_Util.runQuery(query);
+
+        actualColumnList = DB_Util.getAllColumnNamesAsList();
+
+       System.out.println("actualColumnList = " + actualColumnList);
+
+    }
+    @Then("verify the below columns are listed in result")
+    public void verify_the_below_columns_are_listed_in_result(List<String> expectedColumns) {
+        System.out.println("expectedColumns = " + expectedColumns);
+
+        Assert.assertEquals(expectedColumns,actualColumnList);
+    }
 }
--- target/test-classes/queries/library.sql
+++ target/test-classes/queries/library.sql
@@ -1,3 +1,9 @@
 select count(id) from users;  -- actual
 
 select count(distinct id) from users; -- expected
+
+select * from users;
+
+-- US2
+select count(*) from book_borrow
+where is_returned=0;
